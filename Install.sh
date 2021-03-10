fad0391acc165e61ed2fc6dc208e0f36af2b61bf
SL Phone Technician <77476778+Bithusha@users.noreply.github.com>
2021-03-10 17:08:19 +0530

    Add files via upload

---
+++ Install.sh
@@ -0,0 +1,9 @@
+clear
+pkg install python
+pkg install w3m
+pkg install wget
+pkg install curl
+pkg install ngrok
+pip install requests
+clear
+python File
---
+++ Tool.py
@@ -0,0 +1,18 @@
+import os,sys,time,random
+os.system("clear")
+os.system("xdg-open https://youtube.com/channel/UCWNRCD8AS0MXcW5CrqC3EEQ")
+os.system("toilet -f mono12 -F border Tool")
+print("1.Install Tools")
+print("2.About We")
+print("3.Exit")
+print("___________________________________")
+marks=int(input("Enter number>>"))
+if(marks==1):
+   os.system("python File")
+elif(marks==2):
+   os.system("cat README.md")
+elif(marks==3):
+   os.system("exit")
+   print("Thanks for use this tool")
+elif(marks>=4):
+   os.system("echo Wrong option! && python Tool.py")
